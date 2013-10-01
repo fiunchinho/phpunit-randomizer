f9c3e393e1f08ce04982740a0aa33df7ebaf25ff
fiunchinho <github@armesto.net>
2013-10-01 18:16:20 +0000

    Adding composer autoload

--- bin/phpunit.php
+++ bin/phpunit.php
@@ -1,12 +1,8 @@
 #!/usr/bin/env php
 <?php
-
-define('PHPUnit_MAIN_METHOD', 'PHPUnit_TextUI_Command::main');
-
-if (strpos('@php_bin@', '@php_bin') === 0) {
-    require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'PHPUnit' . DIRECTORY_SEPARATOR . 'Autoload.php';
-} else {
-    require '@php_dir@' . DIRECTORY_SEPARATOR . 'PHPUnit' . DIRECTORY_SEPARATOR . 'Autoload.php';
-}
+// Including composer autoload
+require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+// Including phpunit autoload
+require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'phpunit' . DIRECTORY_SEPARATOR . 'phpunit' . DIRECTORY_SEPARATOR . 'PHPUnit' . DIRECTORY_SEPARATOR . 'Autoload.php';
 
 \PHPUnitRandomizer\PHPUnitRandomizerCommand::main();
