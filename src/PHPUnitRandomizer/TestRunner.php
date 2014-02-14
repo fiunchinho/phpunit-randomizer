9ea97e76ea232235df700eb631998d19817e732e
Victor Sollerhed <victor.sollerhed@ticnet.se>
2014-02-14 23:59:09 +0100

    Adding "--seed <seed>" parameter for seeding randomizer. Also prints seed.

--- src/PHPUnitRandomizer/TestRunner.php
+++ src/PHPUnitRandomizer/TestRunner.php
@@ -3,9 +3,15 @@ namespace PHPUnitRandomizer;
 
 class TestRunner extends \PHPUnit_TextUI_TestRunner
 {
+    public function __construct(PHPUnit_Runner_TestSuiteLoader $loader = NULL, PHP_CodeCoverage_Filter $filter = NULL, $seed = null)
+    {
+        parent::__construct($loader, $filter);
+        $this->seed = $seed;
+    }
+
 	public function doRun(\PHPUnit_Framework_Test $suite, array $arguments = array())
     {
-        $test 	= new Decorator( $suite );
+        $test 	= new Decorator($suite, $this->seed);
         $suite 	= new \PHPUnit_Framework_TestSuite();
         $suite->addTest($test);
 
